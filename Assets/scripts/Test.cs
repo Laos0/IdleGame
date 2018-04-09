182a062442db03de5293095076e271705a1c4aaa
C-EC8EB5A41BCA\Student <isonylao@gmail.com>
2018-04-08 23:00:11 -0500

    added prefab: testCube and script: testScript so unity doesn't remove the folders

---
+++ Assets/scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/// <summary>
+///  DON'T DELETE THIS SCRIPT!
+/// </summary>
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
